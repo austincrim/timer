c1ec53fda355db57f6111addb42194744b6dfb33
Austin <aust.crim@gmail.com>
2020-07-06 18:27:10 -0500

    layout and styling

--- src/App.svelte
+++ src/App.svelte
@@ -19,64 +19,86 @@
         timerOn = true;
         timerInterval = setInterval(() => (currentTime -= 1000), 1000);
     }
+
+    function formatTimer(time) {
+        const minutes = new Date(time).getMinutes();
+        const seconds = new Date(time).getSeconds();
+        let formatted = `${minutes}:`;
+        if (seconds === 0) {
+            formatted += "00";
+        } else if (seconds < 10) {
+            formatted += `0${seconds}`;
+        } else {
+            formatted += `${seconds}`;
+        }
+        return formatted;
+    }
 </script>
 
 <svelte:head>
     <link
         href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css"
         rel="stylesheet" />
+    <title>Pomodoro Timer</title>
 </svelte:head>
 
-<header class='py-3 text-center text-2xl bg-red-100 border-b-2 border-red-200'>
-	<h1>Svelte Pomodoro Timer</h1>
-</header>
+<div class='bg-gray-200 h-screen'>
 
-<main class="flex flex-col justify-center items-center p-8 bg-red-100">
-    <div class="text-6xl font-bold">
-        {new Date(currentTime).getMinutes()}:{new Date(currentTime).getSeconds() === 0 ? new Date(currentTime).getSeconds() + '0' : new Date(currentTime).getSeconds()}
-    </div>
+    <header
+        class="py-3 text-center text-2xl bg-red-100 shadow-lg">
+        <h1>Svelte Pomodoro Timer</h1>
+    </header>
 
-    <div class="space-y-4 flex flex-col">
-        {#if !timerOn}
-            <button
-                class="px-3 py-1 bg-red-300 border-none rounded hover:bg-red-400
-                transition-all duration-100 ease-out"
-                on:click={startTimer}
-                type="button">
-                Start Timer
-            </button>
-        {:else}
-            <button
-                class="px-3 py-1 bg-red-300 border-none rounded hover:bg-red-400
-                transition-all duration-100 ease-out"
-                on:click={pauseTimer}
-                type="button">
-                Pause Timer
-            </button>
-        {/if}
+    <main
+        class="flex flex-col justify-center items-center md:w-3/4 mx-auto mt-24 p-8
+        bg-red-100 rounded shadow-md ">
+        <div class="text-6xl font-bold">{formatTimer(currentTime)}</div>
 
-        <div class='flex space-x-2'>
-            <button
-                class="px-3 py-1 bg-red-300 border-none rounded hover:bg-red-400
-                transition-all duration-100 ease-out"
-                on:click={() => setTimer(DEFAULT_DURATION)}
-                type="button">
-                Pomodoro
-            </button>
-            <button
-                class="px-3 py-1 bg-red-300 border-none rounded hover:bg-red-400
-                transition-all duration-100 ease-out"
-                on:click={() => setTimer(SHORT_BREAK_DURATION)}
-                type="button">
-                Short Break
-            </button>
-            <button
-                class="px-3 py-1 bg-red-300 border-none rounded hover:bg-red-400
-                transition-all duration-100 ease-out"
-                on:click={() => setTimer(LONG_BREAK_DURATION)}
-                type="button">
-                Long Break
-            </button>
+        <div class="space-y-4 flex flex-col">
+            {#if !timerOn}
+                <button
+                    class="px-3 py-1 bg-red-300 border-none rounded
+                    hover:bg-red-400 transition-all duration-100 ease-out"
+                    on:click={startTimer}
+                    type="button">
+                    Start Timer
+                </button>
+            {:else}
+                <button
+                    class="px-3 py-1 bg-red-300 border-none rounded
+                    hover:bg-red-400 transition-all duration-100 ease-out"
+                    on:click={pauseTimer}
+                    type="button">
+                    Pause Timer
+                </button>
+            {/if}
+
+            <div class="flex space-x-2">
+                <button
+                    class="px-3 py-1 bg-red-300 border-none rounded
+                    hover:bg-red-400 transition-all duration-100 ease-out"
+                    on:click={() => setTimer(DEFAULT_DURATION)}
+                    type="button">
+                    Pomodoro
+                </button>
+                <button
+                    class="px-3 py-1 bg-red-300 border-none rounded
+                    hover:bg-red-400 transition-all duration-100 ease-out"
+                    on:click={() => setTimer(SHORT_BREAK_DURATION)}
+                    type="button">
+                    Short Break
+                </button>
+                <button
+                    class="px-3 py-1 bg-red-300 border-none rounded
+                    hover:bg-red-400 transition-all duration-100 ease-out"
+                    on:click={() => setTimer(LONG_BREAK_DURATION)}
+                    type="button">
+                    Long Break
+                </button>
+            </div>
         </div>
-    </div>
-</main>
+    </main>
+    <footer class='mx-auto text-center text-sm text-gray-800 p-6 mt-8 md:w-1/2 rounded bg-gray-100 shadow'>
+        Designed and Developed by Austin Crim
+    </footer>
+</div>
